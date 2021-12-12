795bf2b0790ee14353f0df4768fb0e47a6f92438
arnavthorat78 <arnavthorat78@gmail.com>
2021-12-13 10:09:11 +1100

    Added cards to home page

--- scripts/index/navbar.js
+++ scripts/index/navbar.js
@@ -1,5 +1,18 @@
-$(document).ready(function () {
-	$(".menu-toggle").click(function () {
-		$("nav").toggleClass("active");
+const ready = (fn) => {
+	if (document.readyState != "loading") {
+		fn();
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+};
+const toggle = (ele, name) => {
+	const element = document.querySelector(ele);
+
+	element.classList.contains(name) ? element.classList.remove(name) : element.classList.add(name);
+};
+
+ready(() => {
+	document.querySelector(".menu-toggle").addEventListener("click", () => {
+		toggle("nav", "active");
 	});
 });
